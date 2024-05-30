52fb698cd7ee4d0ec457b034bda93a86e657c91a
Petter Lindh <petter.s.lindh@gmail.com>
2024-05-30 11:08:12 +0200

    1st commit. Structure complete

---
+++ src/main/java/com/example/webservices_individual_blog/WebservicesIndividualBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.webservices_individual_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesIndividualBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebservicesIndividualBlogApplication.class, args);
+    }
+
+}
